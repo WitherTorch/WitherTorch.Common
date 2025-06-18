ad7382bad915df9eb7f352b19054046753d12e4a
米茶 <new1271@outlook.com>
2025-06-18 15:25:40 +0800

    修正 MakeUnsigned 函數

--- WitherTorch.Common/Helpers/MathHelper.Unsigned.cs
+++ WitherTorch.Common/Helpers/MathHelper.Unsigned.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 
 using InlineMethod;
 
@@ -6,19 +7,32 @@ namespace WitherTorch.Common.Helpers
 {
     partial class MathHelper
     {
-        [Inline(InlineBehavior.Keep, export: true)]
-        public static byte MakeUnsigned(sbyte value) => unchecked((byte)Abs(value));
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static byte MakeUnsigned(sbyte value) => unchecked((byte)MakeUnsigned((int)value));
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ushort MakeUnsigned(short value) => unchecked((ushort)MakeUnsigned((int)value));
 
         [Inline(InlineBehavior.Keep, export: true)]
-        public static ushort MakeUnsigned(short value) => unchecked((ushort)Abs(value));
+        public static uint MakeUnsigned(int value)
+            => unchecked((uint)(value & (0 - BooleanToInt32(value > 0))));
 
         [Inline(InlineBehavior.Keep, export: true)]
-        public static uint MakeUnsigned(int value) => unchecked((uint)Abs(value));
+        public static ulong MakeUnsigned(long value)
+            => unchecked((uint)(value & (0L - BooleanToInt64(value > 0))));
 
-        [Inline(InlineBehavior.Keep, export: true)]
-        public static ulong MakeUnsigned(long value) => unchecked((ulong)Abs(value));
-
-        [Inline(InlineBehavior.Keep, export: true)]
-        public static nuint MakeUnsigned(nint value) => unchecked((nuint)Abs(value));
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static nuint MakeUnsigned(nint value)
+            => UnsafeHelper.PointerSizeConstant switch
+            {
+                sizeof(int) => MakeUnsigned(unchecked((int)value)),
+                sizeof(long) => unchecked((nuint)MakeUnsigned(unchecked((long)value))),
+                _ => UnsafeHelper.PointerSize switch
+                {
+                    sizeof(int) => MakeUnsigned(unchecked((int)value)),
+                    sizeof(long) => unchecked((nuint)MakeUnsigned(unchecked((long)value))),
+                    _ => throw new NotSupportedException("Unsupported pointer size: " + UnsafeHelper.PointerSize),
+                },
+            };
     }
 }
