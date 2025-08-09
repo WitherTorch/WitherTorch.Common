bf9dac28fb4c26b171753c447e4452ac8e5bb572
米茶 <new1271@outlook.com>
2025-08-10 03:41:19 +0800

    分離 StringSlice 與其子類別所在之檔案

---
+++ WitherTorch.Common/Text/StringSlice.Enumerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace WitherTorch.Common.Text
+{
+    partial struct StringSlice
+    {
+        public struct Enumerator : IEnumerator<char>
+        {
+            private readonly StringBase _original;
+            private readonly nuint _startIndex, _length;
+
+            private bool _initialized;
+            private nuint _index;
+
+            public Enumerator(StringSlice slice)
+            {
+                _original = slice._original;
+                _startIndex = slice._startIndex;
+                _length = slice._length;
+
+                _initialized = false;
+                _index = 0;
+            }
+
+            public readonly char Current
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get
+                {
+                    if (!_initialized)
+                        throw new InvalidOperationException("Enumerator not initialized. Call MoveNext() first.");
+                    nuint index = _index;
+                    if (index >= _length)
+                        throw new InvalidOperationException("Enumerator has reached the end of the collection.");
+                    return _original.GetCharAt(_startIndex + index);
+                }
+            }
+
+            readonly object IEnumerator.Current => Current;
+
+            public readonly void Dispose() { }
+
+            public bool MoveNext()
+            {
+                if (!_initialized)
+                {
+                    _initialized = true;
+                    _index = 0;
+                    return _length > 0;
+                }
+
+                nuint nextIndex = _index + 1;
+                if (nextIndex >= _length)
+                    return false;
+
+                _index = nextIndex;
+                return true;
+            }
+
+            public void Reset() => _initialized = false;
+        }
+    }
+}
--- WitherTorch.Common/Text/StringSlice.cs
+++ WitherTorch.Common/Text/StringSlice.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
 using WitherTorch.Common.Helpers;
 
 namespace WitherTorch.Common.Text
 {
-    public readonly struct StringSlice : IEnumerable<char>
+    public readonly partial struct StringSlice : IEnumerable<char>
     {
         public static readonly StringSlice Empty = new StringSlice(StringBase.Empty, 0u, 0u);
 
@@ -108,6 +107,12 @@ namespace WitherTorch.Common.Text
 #endif
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(in StringSlice left, in StringSlice right) => left.Equals(right);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(in StringSlice left, in StringSlice right) => !left.Equals(right);
+
         public bool Equals(StringSlice other)
             => ReferenceEquals(_original, other._original) && _startIndex == other._startIndex && _length == other._length;
 
@@ -129,62 +134,6 @@ namespace WitherTorch.Common.Text
             fixed (char* ptr = result)
                 original.CopyToCore(ptr, startIndex, length);
             return result;
-        }
-
-        public struct Enumerator : IEnumerator<char>
-        {
-            private readonly StringBase _original;
-            private readonly nuint _startIndex, _length;
-
-            private bool _initialized;
-            private nuint _index;
-
-            public Enumerator(StringSlice slice)
-            {
-                _original = slice._original;
-                _startIndex = slice._startIndex;
-                _length = slice._length;
-
-                _initialized = false;
-                _index = 0;
-            }
-
-            public readonly char Current
-            {
-                [MethodImpl(MethodImplOptions.AggressiveInlining)]
-                get
-                {
-                    if (!_initialized)
-                        throw new InvalidOperationException("Enumerator not initialized. Call MoveNext() first.");
-                    nuint index = _index;
-                    if (index >= _length)
-                        throw new InvalidOperationException("Enumerator has reached the end of the collection.");
-                    return _original.GetCharAt(_startIndex + index);
-                }
-            }
-
-            readonly object IEnumerator.Current => Current;
-
-            public readonly void Dispose() { }
-
-            public bool MoveNext()
-            {
-                if (!_initialized)
-                {
-                    _initialized = true;
-                    _index = 0;
-                    return _length > 0;
-                }
-
-                nuint nextIndex = _index + 1;
-                if (nextIndex >= _length)
-                    return false;
-
-                _index = nextIndex;
-                return true;
-            }
-
-            public void Reset() => _initialized = false;
         }
     }
 }
