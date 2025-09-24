45b6b72165f87b4fe0a8783ad9339561642a7a60
米茶 <new1271@outlook.com>
2025-09-24 17:06:03 +0800

    加入 FileDialogExtensions

---
+++ WitherTorch.Common.Windows/Extensions/FileDialogExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+using WitherTorch.Common.Buffers;
+using WitherTorch.Common.Helpers;
+using WitherTorch.Common.Native;
+using WitherTorch.Common.Threading;
+using WitherTorch.Common.Windows.ObjectModels;
+
+namespace WitherTorch.Common.Windows.Extensions
+{
+    public static class FileDialogExtensions
+    {
+        private static readonly LazyTiny<ShellItem> _desktopItemLazy = new LazyTiny<ShellItem>(
+            static () => ShellItem.Create(Environment.SpecialFolder.Desktop), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void SetDefaultFolderAsDesktop(this FileDialog _this)
+            => _this.SetDefaultFolder(_desktopItemLazy.Value);
+
+        public static unsafe void SetFileType(this FileDialog _this, string name, string pattern)
+        {
+            ComDialogFilterSpecification filterSpec = new ComDialogFilterSpecification()
+            {
+                Name = NativeMethods.AllocCStyleUtf16String(name),
+                Specfication = NativeMethods.AllocCStyleUtf16String(pattern)
+            };
+            try
+            {
+                _this.SetFileTypes(1u, &filterSpec);
+            }
+            finally
+            {
+                NativeMethods.FreeMemory(filterSpec.Name);
+                NativeMethods.FreeMemory(filterSpec.Specfication);
+            }
+        }
+
+        public static unsafe void SetFileTypes(this FileDialog _this, params (string name, string pattern)[] fileTypes)
+        {
+            if (fileTypes is null)
+                return;
+
+            int length = fileTypes.Length;
+            if (length <= 0)
+                return;
+
+            uint count = unchecked((uint)length);
+
+            ArrayPool<ComDialogFilterSpecification> pool = ArrayPool<ComDialogFilterSpecification>.Shared;
+            ComDialogFilterSpecification[] buffer = pool.Rent(count);
+
+            ref (string name, string pattern) fileTypeRef = ref fileTypes[0];
+            ref ComDialogFilterSpecification bufferRef = ref buffer[0];
+            try
+            {
+                for (uint i = 0; i < count; i++)
+                {
+                    (string name, string pattern) = UnsafeHelper.AddTypedOffset(ref fileTypeRef, i);
+                    UnsafeHelper.AddTypedOffset(ref bufferRef, i)
+                        = new ComDialogFilterSpecification()
+                        {
+                            Name = NativeMethods.AllocCStyleUtf16String(name),
+                            Specfication = NativeMethods.AllocCStyleUtf16String(pattern)
+                        };
+                }
+                fixed (ComDialogFilterSpecification* ptr = buffer)
+                    _this.SetFileTypes(count, ptr);
+            }
+            finally
+            {
+                for (uint i = 0; i < count; i++)
+                {
+                    ComDialogFilterSpecification specification = UnsafeHelper.AddTypedOffset(ref bufferRef, i);
+                    NativeMethods.FreeMemory(specification.Name);
+                    NativeMethods.FreeMemory(specification.Specfication);
+                }
+                pool.Return(buffer);
+            }
+        }
+    }
+}
